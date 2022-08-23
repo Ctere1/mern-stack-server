45c6dbc1fe4925096f85109c148499ca9428d3b4
Cemil Tan <cemiltan1999@gmail.com>
2022-08-23 05:53:24 +0300

    room middleware

--- routes/roomRoutes.js
+++ routes/roomRoutes.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 
 const { getAllRooms } = require('../controllers/roomController');
+const validateToken = require('../middleware/validateToken');
 
 //Get all rooms
-router.get("", getAllRooms)
+router.get("", validateToken, getAllRooms)
 
 module.exports = router
